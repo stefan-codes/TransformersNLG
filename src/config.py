46c48503957cf07d8264272f8ca6467b25297ccd
Stefan <stefan.hristov32@gmail.com>
2020-03-05 18:37:30 +0000

    Updated the way the bleu is calculated, merged train and evaluate

--- src/config.py
+++ src/config.py
@@ -15,9 +15,9 @@ FILTER_BY_LENGTH = False
 EXAMPLES_MAX_LENGTH = 200    # To keep this example small and relatively fast, drop examples with a length of over 40 tokens.
 BATCH_SIZE = 64
 SHUFFLE_BUFFER_SIZE = 43_000     # For perfect shuffling, a buffer size greater than or equal to the full size of the dataset is required.
-EPOCHS = 0
+EPOCHS = 140
 
-########## Hyperparameters ###########
+########## Hyperparameters ########### vs 48,605,765 params
 num_layers = 6          # 4 vs 6
 d_model = 512           # 128 vs 512
 num_heads = 8           # 8
--- src/mainScript.py
+++ src/mainScript.py
@@ -2,16 +2,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 import sys
 import config
-import tensorflow as tf
-import nltk
-import csv
-import datetime
-# nltk.download('punkt')
+#import tensorflow as tf
+#import datetime
 from modules.input_pipeline import create_input_pipeline
 from modules.transformer import create_transformer
-from modules.train_and_checkpointing import train_the_transformer
-from modules.evaluate import generate_sentence
-from nltk.translate.bleu_score import sentence_bleu
+from modules.train_and_evaluate import train_the_transformer
+
 
 # mr - meaning representation
 # ref - reference             
@@ -25,53 +21,6 @@ input_pipeline = create_input_pipeline()
 transformer = create_transformer(input_pipeline)
 
 # Train the transformer
-train_the_transformer(transformer, input_pipeline.train_dataset)
-
-# Write to a csv
-# name = datetime.datetime.now()
-if not os.path.isdir(config.results_path):
-    os.makedirs(config.results_path)
-file_name = '{}/{}.csv'.format(config.results_path, 'results')
-with open(file_name, 'a+', newline='') as csv_file:
-
-    header_names = ['mr', 'ref', 'prediction', '1-gram', '2-gram', '3-gram', '4-gram']
-    the_writer = csv.DictWriter(csv_file, fieldnames=header_names)
-
-    the_writer.writeheader()
-
-    counter = 0
-    # Evaluate
-    for entry in input_pipeline.test_examples:
-        # Get a prediction
-        mr,ref = entry
-        mr_example = str(mr.numpy(), 'utf-8')
-        ref_example = str(ref.numpy(), 'utf-8')
-        predicted_sentence = generate_sentence(mr_example, input_pipeline, transformer)
-
-        # Get the bleu scores   
-        prediction = nltk.word_tokenize(predicted_sentence)
-        reference = nltk.word_tokenize(ref_example)
-        list_references = []
-        list_references.append(reference)
-        one_gram = sentence_bleu(list_references, prediction, weights=(1,0,0,0))
-        two_gram = sentence_bleu(list_references, prediction, weights=(0,1,0,0))
-        three_gram = sentence_bleu(list_references, prediction, weights=(0,0,1,0))
-        four_gram = sentence_bleu(list_references, prediction, weights=(0,0,0,1))
-
-        # write to the file
-        the_writer.writerow({'mr' : mr_example, 'ref' : ref_example, 'prediction' : predicted_sentence, 
-                            '1-gram' : '%.4f' % one_gram, '2-gram' : '%.4f' % two_gram, '3-gram' : '%.4f' % three_gram, '4-gram' : '%.4f' % four_gram})
-
-        counter = counter + 1
-        print(counter)
-        if counter % 10 == 0 :
-            sys.exit()
-    # print('Individual 1-gram: %.4f' % one_gram)
-    # print('Individual 2-gram: %.4f' % two_gram)
-    # print('Individual 3-gram: %.4f' % three_gram)
-    # print('Individual 4-gram: %.4f' % four_gram)
-    # break
-
-
+train_the_transformer(transformer, input_pipeline)
 
 print("Done so far!")
